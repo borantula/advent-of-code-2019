c5ee010a967b080d5a1df7eea85483e924874541
Bora Yalcin <b.yalcin@catawiki.nl>
2019-12-03 22:27:48 +0100

    day 3 solution

--- src/day1/index.spec.js
+++ src/day1/index.spec.js
@@ -2,20 +2,19 @@ import {
   calculateFuelMass,
   calculateRequiredFuelMass,
   calculateFuelTotalForModules
-} from './index';
+} from "./index";
 
-test('calculateFuelMass', () => {
+test("calculateFuelMass", () => {
   expect(calculateFuelMass(12)).toBe(2);
   expect(calculateFuelMass(114739)).toBe(38244);
   expect(calculateFuelMass(3)).toBe(0);
 });
 
-
-test('calculateRequiredFuelMass', () => {
+test("calculateRequiredFuelMass", () => {
   expect(calculateRequiredFuelMass(100756)).toBe(50346);
   expect(calculateRequiredFuelMass(50378)).toBe(25158);
 });
 
-test('calculateFuelTotalForModules', () => {
-  expect(calculateFuelTotalForModules([100756,50378])).toBe(50346+25158);
+test("calculateFuelTotalForModules", () => {
+  expect(calculateFuelTotalForModules([100756, 50378])).toBe(50346 + 25158);
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 // TODO: do something nicer for the next days
 // import './day1/index';
-import "./day2/index";
+// import "./day2/index";
+import "./day3/index";
